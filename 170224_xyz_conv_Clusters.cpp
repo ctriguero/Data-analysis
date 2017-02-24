0cbca7c4aeca50816513110ee0a92db33faa82d6
ctriguero <carles.triguero@gmail.com>
2017-02-24 14:49:11 +0000

    Add files via upload

---
+++ 170224_xyz_conv_Clusters.cpp
@@ -0,0 +1,151 @@
+// classes example
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <ctime>
+#include <unistd.h>
+#include <cstdlib>
+#include <sstream>
+#include <cmath>
+#include <ctime>
+#include <algorithm> // use abs() needed for sort
+
+//#include <string> // 
+//#include<math.h> // use sqrt()
+
+
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+using namespace std;
+
+
+///
+/// 	sed -ie 's/X/1/g' Cluster1.xyz
+///	sed -ie 's/atoms in 1 th largest cluster//g' Cluster1.xyz
+///
+
+//**********************************************************************************
+// main function
+//**********************************************************************************
+int main( int argc, const char* argv[] )
+{
+	std::cout << endl ;
+	std::cout << BOLDYELLOW << "    _________________________________________________" << std::endl ;
+	std::cout << BOLDYELLOW << "            _=_                                      " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "          q(-_-)p                                    " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "          '_) (_`         CONVERTING XYZ FILES       " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "          /__/  \\         Carles Triguero 2017      " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "        _(<_   / )_                                  " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "       (__\\_\\_|_/__)                               " << RESET << std::endl ;
+	std::cout << BOLDYELLOW << "    _________________________________________________" << RESET << std::endl ;
+	std::cout << endl ;
+
+	// Check the number of arguments (1 means only the name of the program => No argument)
+	if ( argc == 1 )
+	{
+		cout << "\t Wrong usage. You should execute:" << endl ;
+		cout << BOLDRED << "\t " << argv[0] << " [File to analyze.dat]" << RESET << endl ;
+		cout << " " << endl ;
+		return (0) ;
+	}
+	
+	// Check if the file exists
+	ifstream file(argv[1]) ;
+	if (!file)
+	{
+    	std::cout << BOLDRED <<"    -> ERROR: File test.xyz does not exist. ABORTING" << RESET << std::endl ;
+    	std::cout << std::endl ;
+		abort () ;
+	}
+	file.close() ;
+	
+	std::cout << YELLOW << "    -> File " << BOLDYELLOW << argv[1] << RESET << YELLOW << " found" << RESET << std::endl ;
+
+	
+	// Count execution time
+	int start_s=clock();
+
+	// Preparing output file
+	std::string name = argv[1];
+	//std::stringstream name = argv[1];
+	cout << name << endl ;
+	name.erase (name.end()-4, name.end()) ;
+	cout << name << endl ;
+	std::string nameout ;
+	nameout = name + "_Ang.xyz"  ;
+	cout << nameout << endl ;	
+
+	
+	// Files to store data
+	ofstream OUT ;
+//	OUT.open ("Cluster3_Ang.xyz", ios::out | ios::trunc); // All computed densities inside cylinders
+	OUT.open (nameout.c_str(), ios::out | ios::trunc); 
+
+	
+	std::string File(argv[1]); // set up a stringstream variable named convert, initialized with the input from argv[1]
+	
+
+	cout << YELLOW << "    -> Processing file: " << BOLDYELLOW << File << RESET << endl ;
+	
+	std::ifstream InputFile ;
+	InputFile.open( File.c_str(), ios::in );
+
+	std::string line ;			// variable string to read the line
+	unsigned int element, atoms ;
+	double x, y, z, f = 10.0 ;
+	
+
+	while (getline(InputFile, line))
+	{
+		std::stringstream aa(line) ; //procesar primera línea (leemos new_s)
+
+		if (line.length() > 10)
+		{		
+			aa >> element >> x >> y >> z ;
+			OUT << element << "  " << f*x << "  " << f*y << "  " << f*z << std::endl ;
+		}
+		if ( ( line.length() < 5 ) && ( line.length() > 1 ) )
+		{
+			aa >> atoms ;
+			OUT<< atoms << std::endl ;	
+			OUT<< std::endl ;
+			getline(InputFile, line) ; //procesar segunda línea (sin hacer nada)	
+		}
+	}
+	InputFile.close() ;
+	OUT.close() ;
+
+	
+
+	// End counting time 	
+ 	int stop_s=clock();
+ 	
+	// Execution time
+ 	std::cout << endl ;
+	std::cout  << YELLOW << "    -> Execution time: " << BOLDYELLOW << ( stop_s - start_s )/double(CLOCKS_PER_SEC) << RESET << YELLOW << " seconds" << std::endl ;
+	std::cout << endl ;
+	std::cout  << BOLDYELLOW << "    Program finished" << RESET << std::endl ;
+	std::cout << endl ;
+	
+	return (0) ;
+}
+
